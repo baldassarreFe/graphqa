938743ed5c3335a8b9fd35bf2c122479a3f72fc9
Federico Baldassarre <baldassarre.fe@gmail.com>
2019-05-28 09:24:23 +0200

    Encoding block of the GN now uses 2 linear layer instead of 1

--- src/proteins/networks.py
+++ src/proteins/networks.py
@@ -6,29 +6,46 @@ import torchgraphs as tg
 
 
 class ProteinGN(nn.Module):
-    def __init__(self, hops=1):
+    def __init__(self, node_features, edge_features, aggregation='mean', hops=1):
         super().__init__()
         self.hops = hops
 
-        self.encoder = nn.Sequential(OrderedDict([
-            ('edge', tg.EdgeLinear(out_features=8, edge_features=1, sender_features=83)),
-            ('edge_relu', tg.EdgeReLU()),
-            ('node', tg.NodeLinear(out_features=16, incoming_features=8, node_features=83, aggregation='mean')),
-            ('node_relu', tg.NodeReLU()),
-            ('global', tg.GlobalLinear(out_features=4, edge_features=8, node_features=16, aggregation='mean')),
-            ('global_relu', tg.GlobalReLU()),
-        ]))
+        # Edge feature shape: 1 -> 4 -> 8
+        # Node feature shape: 83 -> 32 -> 16
+        # Global feature shape: None -> 4
+        self.encoder = nn.Sequential(
+            nn.Sequential(OrderedDict({
+                'edge': tg.EdgeLinear(out_features=4, edge_features=edge_features),
+                'edge_relu': tg.EdgeReLU(),
+                'node': tg.NodeLinear(out_features=32, node_features=node_features),
+                'node_relu': tg.NodeReLU(),
+            })),
+            nn.Sequential(OrderedDict({
+                'edge': tg.EdgeLinear(out_features=8, edge_features=4),
+                'edge_relu': tg.EdgeReLU(),
+                'node': tg.NodeLinear(out_features=16, node_features=32),
+                'node_relu': tg.NodeReLU(),
+                'global': tg.GlobalLinear(node_features=16, out_features=4, aggregation='mean'),
+            }))
+        )
 
+        # Edge feature shape: 8 -> 8
+        # Node feature shape: 16 -> 16
+        # Global feature shape: 4 -> 4
         self.hidden = nn.Sequential(OrderedDict({
-            'edge': tg.EdgeLinear(out_features=8, edge_features=8, sender_features=16, global_features=4),
+            'edge': tg.EdgeLinear(
+                out_features=8, edge_features=8, sender_features=16, global_features=4),
             'edge_relu': tg.EdgeReLU(),
-            'node': tg.NodeLinear(out_features=16, node_features=16, incoming_features=8,
-                                  global_features=4, aggregation='mean'),
+            'node': tg.NodeLinear(
+                out_features=16, node_features=16, incoming_features=8, global_features=4, aggregation=aggregation),
             'node_relu': tg.NodeReLU(),
-            'global': tg.GlobalLinear(4, edge_features=8, node_features=16, global_features=4, aggregation='mean'),
+            'global': tg.GlobalLinear(
+                out_features=4, edge_features=8, node_features=16, global_features=4, aggregation=aggregation),
             'global_relu': tg.GlobalReLU(),
         }))
 
+        # Node feature shape: 16 -> 1
+        # Global feature shape: 4 -> 1
         self.readout_nodes = nn.Sequential(OrderedDict({
             'node': tg.NodeLinear(1, node_features=16),
             'node_sigmoid': tg.NodeSigmoid()
