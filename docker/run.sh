c856b07eb862383394c6243763f8f3057748c999
Federico Baldassarre <baldassarre.fe@gmail.com>
2019-05-29 21:57:33 +0200

    More docker configuration and wrapping

--- docker/run.sh
+++ docker/run.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-ENTRYPOINT_PATH="$(realpath ./entrypoint.sh)"
-SOURCE_PATH="$(realpath ..)"
-DATA_PATH="$(realpath ~/experiments/proteins/data)"
-RUNS_PATH="$(realpath ~/experiments/proteins/runs)"
+ENTRYPOINT_PATH="${ENTRYPOINT_PATH-$(realpath ./entrypoint.sh)}"
+SOURCE_PATH="${SOURCE_PATH-$(realpath ..)}"
+DATA_PATH="${DATA_PATH-$(realpath ~/experiments/proteins/data)}"
+RUNS_PATH="${RUNS_PATH-$(realpath ~/experiments/proteins/runs)}"
 
 docker run --runtime=nvidia --ipc=host --rm -it \
-    --workdir='/root/proteins' \
-    --mount "type=bind,source=${SOURCE_PATH},target=/root/proteins" \
-    --mount "type=bind,source=${DATA_PATH},target=/root/experiments/proteins/data,readonly" \
-    --mount "type=bind,source=${RUNS_PATH},target=/root/experiments/proteins/runs" \
+    --workdir='/proteins' \
+    --env="EXTERNAL_USER=$(id -u)" \
+    --mount "type=bind,source=${SOURCE_PATH},target=/proteins" \
+    --mount "type=bind,source=${DATA_PATH},target=/data,readonly" \
+    --mount "type=bind,source=${RUNS_PATH},target=/runs" \
     --mount="type=bind,source=${ENTRYPOINT_PATH},target=/entrypoint.sh" \
-    --entrypoint=/entrypoint.sh --env="EXTERNAL_USER=$(id -u)" \
+    --entrypoint=/entrypoint.sh \
     proteins:v0 \
     "$@"
