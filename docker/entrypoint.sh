e3e0136456c784f4f5aa9e92bb5cef5abdad5679
Federico Baldassarre <baldassarre.fe@gmail.com>
2019-07-05 13:46:33 +0200

    Docker entrypoint now returns the exit status of the training script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,8 @@ EXTERNAL_USER=${EXTERNAL_USER-$(whoami)}
 echo "Docker user: $(whoami)@$(hostname)"
 echo "External user: ${EXTERNAL_USER}"
 
+nvidia-smi
+
 source /root/.zshrc
 conda activate base
 pip install /root/proteins > /dev/null && echo "Installed proteins@$(cd /root/proteins && git rev-parse --short HEAD)"
@@ -11,10 +13,14 @@ pip install /root/proteins > /dev/null && echo "Installed proteins@$(cd /root/pr
 if [[ -n "$@" ]]; then
     echo -e "Executing CMD: $@\n"
     zsh -c "$@"
+    OUT_CODE=$?
 else
     echo -e "Starting a zsh shell...\n"
     zsh
+    OUT_CODE=$?
 fi
 
 # Fix permissions for artifacts created as root
 chown --from=$(id -u):$(id -g) --recursive --changes ${EXTERNAL_USER}:${EXTERNAL_USER} /root/experiments/proteins/runs
+
+exit "$OUT_CODE"
