174ec5cdd80ea52c85a382379eb61ba7bae2eb2e
Federico Baldassarre <baldassarre.fe@gmail.com>
2019-07-09 17:03:47 +0200

    Model now accepts input and output sizes for the hidden graph layers

--- src/proteins/networks.py
+++ src/proteins/networks.py
@@ -2,6 +2,7 @@ import math
 from collections import OrderedDict
 import torch
 from torch import nn
+import numpy as np
 
 import torchgraphs as tg
 
@@ -10,48 +11,53 @@ class ProteinGN(nn.Module):
     def __init__(
             self,
             layers=1,
-            size_edges=2,
-            size_nodes=4,
-            size_globals=1,
+            hidden_size_in_edges=8,
+            hidden_size_in_nodes=16,
+            hidden_size_in_globals=4,
+            hidden_size_out_edges=64,
+            hidden_size_out_nodes=128,
+            hidden_size_out_globals=32,
             dropout=0
     ):
         super().__init__()
         self.layers = []
 
-        # Edge feature shape: 2 -> 8 -> 32
-        # Node feature shape: 83 -> 64 -> 64
-        # Global feature shape: None -> 8
+        # Edge feature shape: 2 -> hidden_size_in_edges//2 -> hidden_size_in_edges
+        # Node feature shape: 83 -> hidden_size_in_nodes -> hidden_size_in_nodes//2
+        # Global feature shape: None -> hidden_size_in_globals
         self.encoder = nn.Sequential(OrderedDict({
-            'edge1': tg.EdgeLinear(out_features=8, edge_features=2),
+            'edge1': tg.EdgeLinear(out_features=hidden_size_in_edges//2, edge_features=2),
             'edge1_relu': tg.EdgeReLU(),
             'edge1_dropout': tg.EdgeDropout(p=dropout),
-            'edge2': tg.EdgeLinear(out_features=32, edge_features=8),
+            'edge2': tg.EdgeLinear(out_features=hidden_size_in_edges, edge_features=hidden_size_in_edges//2),
             'edge2_relu': tg.EdgeReLU(),
             'edge2_dropout': tg.EdgeDropout(p=dropout),
 
-            'node1': tg.NodeLinear(out_features=64, node_features=83),
+            'node1': tg.NodeLinear(out_features=hidden_size_in_nodes//2, node_features=83),
             'node1_relu': tg.NodeReLU(),
             'node1_dropout': tg.NodeDropout(p=dropout),
-            'node2': tg.NodeLinear(out_features=64, node_features=64),
+            'node2': tg.NodeLinear(out_features=hidden_size_in_nodes, node_features=hidden_size_in_nodes//2),
             'node2_relu': tg.NodeReLU(),
             'node2_dropout': tg.NodeDropout(p=dropout),
 
-            'global': tg.GlobalLinear(out_features=8, bias=True),
+            'global': tg.GlobalLinear(out_features=hidden_size_in_globals, bias=True),
             'global_relu': tg.GlobalReLU(),
             'global_dropout': tg.GlobalDropout(p=dropout),
         }))
 
-        # Edge, node and global shapes linearly decrease from (32, 64, 8)
-        # to the sizes given as parameters (e.g. 2, 4, 1)
-        # in the number of steps given as parameter (e.g. 10).
-        hidden_size_edges = torch.linspace(32, size_edges, layers+1).int().tolist()
-        hidden_size_nodes = torch.linspace(64, size_nodes, layers+1).int().tolist()
-        hidden_size_globals= torch.linspace(8, size_globals, layers+1).int().tolist()
+        # Edge, node and global shapes decrease from
+        # (hidden_size_in_edges, hidden_size_in_nodes, hidden_size_in_globals)
+        # to
+        # (hidden_size_out_edges, hidden_size_out_nodes, hidden_size_out_globals)
+        # following powers of 2 in the number of steps given as parameter (e.g. 10).
+        hidden_layers_output_sizes = np.power(2, np.linspace(
+            np.log2((hidden_size_in_edges, hidden_size_in_nodes, hidden_size_in_globals)),
+            np.log2((hidden_size_out_edges, hidden_size_out_nodes, hidden_size_out_globals)),
+            num=layers, endpoint=False
+        ).round().astype(int)).tolist()[1:] + [(hidden_size_out_edges, hidden_size_out_nodes, hidden_size_out_globals)]
 
-        for in_e, in_n, in_g, out_e, out_n, out_g in zip(
-            hidden_size_edges, hidden_size_nodes, hidden_size_globals,
-            hidden_size_edges[1:], hidden_size_nodes[1:], hidden_size_globals[1:]
-        ):
+        in_e, in_n, in_g = hidden_size_in_edges, hidden_size_in_nodes, hidden_size_in_globals
+        for out_e, out_n, out_g in hidden_layers_output_sizes:
             layer = nn.Sequential(OrderedDict({
                 'edge1': tg.EdgeLinear(
                     out_features=out_e,
@@ -61,12 +67,6 @@ class ProteinGN(nn.Module):
                 ),
                 'edge1_relu': tg.EdgeReLU(),
                 'edge1_dropout': tg.EdgeDropout(p=dropout),
-                # 'edge2': tg.EdgeLinear(
-                #     out_features=out_e,
-                #     edge_features=out_e,
-                # ),
-                # 'edge2_relu': tg.EdgeReLU(),
-                # 'edge2_dropout': tg.EdgeDropout(p=dropout),
 
                 'node1': tg.NodeLinear(
                     out_features=out_n,
@@ -77,12 +77,6 @@ class ProteinGN(nn.Module):
                 ),
                 'node1_relu': tg.NodeReLU(),
                 'node1_dropout': tg.NodeDropout(p=dropout),
-                # 'node2': tg.NodeLinear(
-                #     out_features=out_n,
-                #     node_features=out_n,
-                # ),
-                # 'node2_relu': tg.NodeReLU(),
-                # 'node2_dropout': tg.NodeDropout(p=dropout),
 
                 'global1': tg.GlobalLinear(
                     out_features=out_g,
@@ -93,23 +87,19 @@ class ProteinGN(nn.Module):
                 ),
                 'global1_relu': tg.GlobalReLU(),
                 'global1_dropout': tg.GlobalDropout(p=dropout),
-                # 'global2': tg.GlobalLinear(
-                #     out_features=out_g,
-                #     global_features=out_g,
-                # ),
-                # 'global2_relu': tg.GlobalReLU(),
-                # 'global2_dropout': tg.GlobalDropout(p=dropout),
             }))
             self.layers.append(layer)
+            in_e, in_n, in_g = out_e, out_n, out_g
 
         self.layers = torch.nn.Sequential(OrderedDict({f'layer_{i}': l for i, l in enumerate(self.layers)}))
 
-        # Node feature shape: size_nodes -> 1
-        # Global feature shape: size_globals -> 1
+        # Node feature shape: hidden_size_out_nodes -> 1
+        # Global feature shape: hidden_size_out_globals -> 1
         self.readout = nn.Sequential(OrderedDict({
-            'node': tg.NodeLinear(1, node_features=size_nodes),
+            'node': tg.NodeLinear(1, node_features=hidden_size_out_nodes),
             'node_sigmoid': tg.NodeSigmoid(),
-            'global': tg.GlobalLinear(1, global_features=size_globals, node_features=1, aggregation='mean'),
+            # 'global': tg.GlobalLinear(1, global_features=hidden_size_out_globals, node_features=1, aggregation='mean')
+            'global': tg.GlobalLinear(1, global_features=hidden_size_out_globals),
             'global_sigmoid': tg.GlobalSigmoid()
         }))
 
