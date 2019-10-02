292cf05c49421b42360aaa496071e180ca2fb496
Federico Baldassarre <baldassarre.fe@gmail.com>
2019-10-02 13:18:00 +0200

    Updated docker entrypoint (fixed argument order for pip install)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/zsh
 
 EXTERNAL_USER=${EXTERNAL_USER-$(whoami)}
-echo "Docker user  : $(whoami)@$(hostname)"
+echo "Docker user  : $(whoami)"
 echo "External user: ${EXTERNAL_USER}"
+echo "Container id : $(hostname)"
 echo "Working dir  : $(pwd)"
 
 nvidia-smi
@@ -10,7 +11,8 @@ nvidia-smi
 source /root/.zshrc
 conda activate base
 # Install as editable, otherwise pip tries to copy the whole data folder and it takes forever
-pip install --editable --quiet /proteins/src && echo "Installed proteins@$(cd /proteins/src && git rev-parse --short HEAD)"
+pip install --quiet --editable /proteins &&
+echo "Installed proteins@$(cd /proteins && git rev-parse --short HEAD)"
 
 if [[ -n "$@" ]]; then
     echo -e "Executing CMD: $@\n"
