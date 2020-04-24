379d5c156e8ce5c924b5ab656ba8edb6953685e9
Federico Baldassarre <baldassarre.fe@gmail.com>
2020-04-24 10:48:35 +0200

    Increased version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='graphqa',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
